7a8371abc12e4f445b56c479a5b54abdf10807f2
Pola <polaalaa5@gmail.com>
2024-09-25 06:29:12 +0300

    create note model

---
+++ lib/models/note_Model.dart
@@ -0,0 +1,12 @@
+class NoteModel {
+  final String title;
+  final String contant;
+  final String date;
+  final int color;
+
+  NoteModel(
+      {required this.title,
+      required this.contant,
+      required this.date,
+      required this.color});
+}
